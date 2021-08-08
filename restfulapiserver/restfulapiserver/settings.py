827aa78a13b15fa66f9cb2e70d31b98669590d29
choiseonjae <tjswo2292@naver.com>
2021-08-08 15:23:58 +0900

    cart app 생성

--- restfulapiserver/restfulapiserver/settings.py
+++ restfulapiserver/restfulapiserver/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'cart',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
