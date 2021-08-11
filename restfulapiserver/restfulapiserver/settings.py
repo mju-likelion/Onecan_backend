031918474bcdb804427a024bf7779bb1619056e9
qkrthdus605 <qkrthdus605@gmail.com>
2021-08-11 23:28:39 +0900

    heroku

--- restfulapiserver/restfulapiserver/settings.py
+++ restfulapiserver/restfulapiserver/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', my_settings.SECRET_KEY)
+SECRET_KEY = os.environ.get(my_settings.SECRET_KEY, 'SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG', 'True') != 'False')
@@ -165,12 +165,12 @@ REST_FRAMEWORK = {
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', my_settings.AWS_ACCESS_KEY_ID)
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', my_settings.AWS_SECRET_ACCESS_KEY)
+AWS_ACCESS_KEY_ID = os.environ.get(my_settings.AWS_ACCESS_KEY_ID, 'AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get(my_settings.AWS_SECRET_ACCESS_KEY, 'AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'onecandoit'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'ap-northeast-2'
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
