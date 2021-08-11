cb1dea34474cbc4fc33c061ef65f341aff2e9c08
qkrthdus605 <qkrthdus605@gmail.com>
2021-08-12 00:32:27 +0900

    환경변수 수정

--- restfulapiserver/restfulapiserver/settings.py
+++ restfulapiserver/restfulapiserver/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(my_settings.SECRET_KEY, 'SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG', 'True') != 'False')
@@ -165,8 +165,8 @@ REST_FRAMEWORK = {
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_ACCESS_KEY_ID = os.environ.get(my_settings.AWS_ACCESS_KEY_ID, 'AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get(my_settings.AWS_SECRET_ACCESS_KEY, 'AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'onecandoit'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'ap-northeast-2'
