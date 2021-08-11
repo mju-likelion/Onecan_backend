8e6f2d347e0fae58b26c6b6157f7743a91f739ca
qkrthdus605 <qkrthdus605@gmail.com>
2021-08-12 01:02:17 +0900

    ??

---
+++ restfulapiserver/restfulapiserver/mysettings.py
@@ -0,0 +1,3 @@
+SECRET_KEY ='django-insecure-ccapz!g(k^)u-7eq=&@e_$+k#)er5mtok_fp_wj6t*y1^m2mas'
+AWS_ACCESS_KEY_ID = 'AKIAZI324YPVPWSYL47H'
+AWS_SECRET_ACCESS_KEY = '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'
