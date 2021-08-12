103e6ef7dcaa7a497e4e721150b31d18c9234fc9
qkrthdus605 <qkrthdus605@gmail.com>
2021-08-13 03:34:34 +0900

    create pm2/onecandoit.config.js

---
+++ pm2/onecandoit.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps: [{
+    name: "onecandoit",
+    script: "restfulapiserver/manage.py",
+    args: "runserver",
+    interpreter: "/home/ubuntu/myvenv/bin/python",
+  }]
+}
