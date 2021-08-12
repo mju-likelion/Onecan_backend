4f76a0eb87ca90adb9cb8b2be09f5bc8ab4c85d3
yeonghoon1700 <81845350+yeonghoon1700@users.noreply.github.com>
2021-08-12 22:26:03 +0900

    cors 추가

--- restfulapiserver/restfulapiserver/settings.py
+++ restfulapiserver/restfulapiserver/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'rest_auth.registration',
+    'corsheaders',
     'product',
     'recipe',
     'barter',
@@ -55,7 +56,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000' ,
+'http://localhost:3000'] 
+
+CORS_ALLOW_CREDENTIALS = True
+
+
 ROOT_URLCONF = 'restfulapiserver.urls'
 
 TEMPLATES = [
