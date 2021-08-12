90407970955e03179640e7b0c06c6402e5d1f22f
qkrthdus605 <qkrthdus605@gmail.com>
2021-08-12 23:27:52 +0900

    modify settings.py

--- restfulapiserver/restfulapiserver/settings.py
+++ restfulapiserver/restfulapiserver/settings.py
@@ -6,7 +6,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from . import my_settings
+# from . import my_settings
 from pathlib import Path
 import os
 
