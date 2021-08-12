d30697a98077645ef9389cb610d7dc8bcdac2f92
qkrthdus605 <qkrthdus605@gmail.com>
2021-08-12 22:57:42 +0900

    edit settings.py for deploy

--- restfulapiserver/restfulapiserver/settings.py
+++ restfulapiserver/restfulapiserver/settings.py
@@ -17,11 +17,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = my_settings.SECRET_KEY
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ccapz!g(k^)u-7eq=&@e_$+k#)er5mtok_fp_wj6t*y1^m2mas')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = (os.environ.get('DEBUG', 'True') != 'False')
+
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
