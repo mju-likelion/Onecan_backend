a2921df260cdc2d3b4d0eaa69360bb89488bda75
qkrthdus605 <qkrthdus605@gmail.com>
2021-08-12 17:12:58 +0900

    settings.py - middleware

--- restfulapiserver/restfulapiserver/settings.py
+++ restfulapiserver/restfulapiserver/settings.py
@@ -75,9 +75,9 @@ SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware'
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
