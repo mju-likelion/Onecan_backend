022d008b0bbd3612e126348298d1ccd075f77634
qkrthdus605 <qkrthdus605@gmail.com>
2021-08-13 23:43:01 +0900

    cors 경로 수정

--- restfulapiserver/restfulapiserver/settings.py
+++ restfulapiserver/restfulapiserver/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000' ,
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:8000' ,
 'http://localhost:3000'] 
 
 CORS_ALLOW_CREDENTIALS = True
