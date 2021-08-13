4e9790abc8ae6c282a0a0f95f2f844264ba5bc36
qkrthdus605 <qkrthdus605@gmail.com>
2021-08-13 18:41:36 +0900

    SECRET_KEY 숨김 처리

--- restfulapiserver/restfulapiserver/settings.py
+++ restfulapiserver/restfulapiserver/settings.py
@@ -6,7 +6,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-# from . import my_settings
+from . import my_settings
 from pathlib import Path
 import os
 
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ccapz!g(k^)u-7eq=&@e_$+k#)er5mtok_fp_wj6t*y1^m2mas')
+SECRET_KEY = os.environ.get('SECRET_KEY', my_settings.SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = (os.environ.get('DEBUG', 'True') != 'False')
